ad80648e4c5fbd626888c1492e57a89adc13c811
murase_syuka <murase.syuka+github@gmail.com>
2013-09-11 01:50:24 +0900

    cp : hello_fix1.c

---
+++ sample_hello_mruby/hello_fix2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Include the mruby header */
+#include <mruby.h>
+#include <mruby/compile.h>
+#include <mruby/string.h>
+
+int main(void)
+{
+  mrb_state *mrb = mrb_open();
+  mrb_value code = mrb_str_new(mrb, "p 'hello world!'", 16);
+  printf("Executing Ruby code from C!\n");
+
+  mrb_load_string(mrb, RSTRING_PTR(code));
+  return 0;
+}
+
