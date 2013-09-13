fb77cf84c35409fa778f2e231829c2b0174178a2
murase_syuka <murase.syuka+github@gmail.com>
2013-09-13 12:06:11 +0900

    fix : String#concat

--- sample_hello_mruby/hello_fix4.c
+++ sample_hello_mruby/hello_fix4.c
@@ -11,11 +11,15 @@ int main(void)
   mrb_state *mrb = mrb_open();
   
   struct RClass* cv = mrb_class_get(mrb, "String");
-  mrb_value code = mrb_str_new(mrb, "p 'hello world!'", 16);
-  mrb_value str = mrb_obj_new(mrb, cv, 1, &code);
+  mrb_value str = mrb_obj_new(mrb, cv, 0, 0);
+  
+  mrb_value head = mrb_str_new(mrb,"p 'hello",8);
+  mrb_value tail = mrb_str_new(mrb," world'",7);
+
+  str = mrb_funcall(mrb, str, "+", 1, head); /* str += head */
+  str = mrb_funcall(mrb, str, "+", 1, tail); /* str += tail */
 
   printf("str#size is %d \n", mrb_funcall(mrb, str, "size", 0, 0));
-
   printf("str is '%s' \n", RSTRING_PTR(str));
 
   mrb_load_string(mrb, RSTRING_PTR(str));
