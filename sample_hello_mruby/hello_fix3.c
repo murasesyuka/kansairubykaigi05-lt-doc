27f3f8a54b255e3878c48d04f10842912d66e3c6
murase_syuka <murase.syuka+github@gmail.com>
2013-09-13 11:31:31 +0900

    fix : like String.new()

--- sample_hello_mruby/hello_fix3.c
+++ sample_hello_mruby/hello_fix3.c
@@ -9,12 +9,16 @@
 int main(void)
 {
   mrb_state *mrb = mrb_open();
+  
+  struct RClass* cv = mrb_class_get(mrb, "String");
   mrb_value code = mrb_str_new(mrb, "p 'hello world!'", 16);
+  mrb_value str = mrb_obj_new(mrb, cv, 1, &code);
 
-  printf("code#size is %d \n", mrb_funcall(mrb, code, "size", 0, 0));
-  printf("code is '%s' \n", RSTRING_PTR(code));
+  printf("str#size is %d \n", mrb_funcall(mrb, str, "size", 0, 0));
 
-  mrb_load_string(mrb, RSTRING_PTR(code));
+  printf("str is '%s' \n", RSTRING_PTR(str));
+
+  mrb_load_string(mrb, RSTRING_PTR(str));
   return 0;
 }
 
